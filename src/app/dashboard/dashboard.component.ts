6ad015175568561774697ff559d4a1b1ed6fa53d
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-02-21 22:52:11 +0200

    Patient folder

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,24 +6,14 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
-<<<<<<< HEAD
-  showAddPatient = false;
-  constructor() {}
-=======
   showAddPatient= false;
   constructor() { }
->>>>>>> c9327e64819d8b8837109b0da5bd2e5cfea88057
 
   ngOnInit() {}
   showAddPatientModal() {
     this.showAddPatient = true;
   }
-<<<<<<< HEAD
-  closeModal(e) {
-    this.showAddPatient = false;
-=======
   closeModal(e){
       this.showAddPatient = false;
->>>>>>> c9327e64819d8b8837109b0da5bd2e5cfea88057
   }
 }
