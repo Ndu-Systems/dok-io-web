7a4565f66420df09189bcc071293afc30bb01216
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-02-24 13:54:50 +0200

    add user form

--- src/app/dashboard/Patient/add-patient/add-patient.component.ts
+++ src/app/dashboard/Patient/add-patient/add-patient.component.ts
@@ -1,69 +1,71 @@
-import { Component, OnInit, Output ,EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms"; //l-f
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-add-patient',
-  templateUrl: './add-patient.component.html',
-  styleUrls: ['./add-patient.component.scss']
+  selector: "app-add-patient",
+  templateUrl: "./add-patient.component.html",
+  styleUrls: ["./add-patient.component.scss"]
 })
 export class AddPatientComponent implements OnInit {
-@Output() closeModalAction:EventEmitter<boolean> = new EventEmitter();
+  @Output() closeModalAction: EventEmitter<boolean> = new EventEmitter();
 
-/*
+  /*
 Form begin here
 */
-rForm: FormGroup;
+  rForm: FormGroup;
 
-FirstName: string;
-Surname: string;
-IdNumber: string;
-DOB: string;
-Gender: string;
-Email: string;
-Cellphone: string;
-AddressLine1: string;
-AddressLine2: string;
-AddressLine3: string;
-City: string;
-PostCode: string;
-CreateUserId: string;
-ModifyUserId: string;
-StatusId: number;
-Province: number;
-//validation
-message: string = '';
+  FirstName: string;
+  Surname: string;
+  IdNumber: string;
+  DOB: string;
+  Gender: string;
+  Email: string;
+  Cellphone: string;
+  AddressLine1: string;
+  AddressLine2: string;
+  AddressLine3: string;
+  City: string;
+  PostCode: string;
+  CreateUserId: string;
+  ModifyUserId: string;
+  StatusId: number;
+  Province: number;
+  //validation
+  message: string = "";
 
-/*
+  /*
 Form ends here
 */
-Age="0";
+  Age = "0";
 
-  constructor(private fb:FormBuilder, private http:HttpClient) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.rForm = fb.group({
-      'FirstName':[null, Validators.required],
-      'Surname':[null, Validators.required],
-      'IdNumber':[null, Validators.required],
-      'DOB':[null, Validators.required],
-      'Gender':[null, Validators.required],
-      'Email':[null, Validators.required],
-      'Cellphone':[null, Validators.required],
-      'AddressLine1':[null, Validators.required],
-      'City':[null, Validators.required],
-      'PostCode':[null, Validators.required],
-      'cell':[null,Validators.required],
-      'Age':[null],
-      'Province':[null],
-      'message':[null,Validators.compose([
-        Validators.required,
-        Validators.minLength(15)
-      ])],
-    })
-   }
-
-  ngOnInit() {
+      FirstName: [null, Validators.required],
+      Surname: [null, Validators.required],
+      IdNumber: [null, Validators.required],
+      DOB: [null, Validators.required],
+      Gender: [null, Validators.required],
+      Email: [null, Validators.required],
+      Cellphone: [null, Validators.required],
+      AddressLine1: [null, Validators.required],
+      City: [null, Validators.required],
+      PostCode: [null, Validators.required],
+      cell: [null, Validators.required],
+      Age: [null],
+      Province: [null],
+      message: [
+        null,
+        Validators.compose([Validators.required, Validators.minLength(15)])
+      ]
+    });
   }
-  closeModal(){
-this.closeModalAction.emit(false);
+
+  ngOnInit() {}
+  closeModal() {
+    this.closeModalAction.emit(false);
+  }
+  register(data) {
+    console.log(data);
   }
 }
