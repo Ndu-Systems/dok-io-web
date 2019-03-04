a872abb27ea0f2ce03563d26f29b38df8821de62
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-03-05 00:09:54 +0200

    form array

--- src/app/dashboard/prescription/add-prescription/add-prescription.component.ts
+++ src/app/dashboard/prescription/add-prescription/add-prescription.component.ts
@@ -1,16 +1,70 @@
-import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { AddPrescriptionModel } from "./../../../models/addprescription.model";
+import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
+import { FormGroup, FormBuilder, FormArray } from "@angular/forms";
+import { getCurrentUser } from "src/app/shared";
 
 @Component({
   selector: "app-add-prescription",
   templateUrl: "./add-prescription.component.html",
   styleUrls: ["./add-prescription.component.scss"]
 })
-export class AddPrescriptionComponent implements OnInit {
+export class AddPrescriptionComponent {
   @Output() closeModalAction: EventEmitter<boolean> = new EventEmitter();
-  constructor() {}
+  @Input() patientId;
+  UserId: string = getCurrentUser();
+
+  rForm: FormGroup;
+  data: AddPrescriptionModel;
+  constructor(private fb: FormBuilder) {
+    this.rForm = this.fb.group({
+      patientId: this.patientId,
+      symptoms: "",
+      diagnosis: "",
+      boolPreasure: "",
+      pulseRate: "",
+      CreateUserId: this.UserId,
+      ModifyUserId: this.UserId,
+      StatusId: 1,
+      drugs: this.fb.array([])
+    });
+    this.rForm.valueChanges.subscribe(data => {
+      console.log(data);
+    });
+  }
   closeModal() {
     this.closeModalAction.emit(true);
   }
-  
-  ngOnInit() {}
+
+  get formDrugs() {
+    return this.rForm.get("drugs") as FormArray;
+  }
+  addDrug(){
+    const drug = this.fb.group({
+      medicationId: '',
+      unit: '',
+      dosage: '',
+    });
+
+    this.formDrugs.push(drug);
+  }
+  deleteDrug(i){
+    this.formDrugs.removeAt(i);
+  }
 }
+
+/*
+    patientId: string;
+    symptoms: string;
+    diagnosis: string;
+    boolPreasure: string;
+    pulseRate: string;
+    CreateUserId: string;
+    ModifyUserId: string;
+    StatusId: string;
+    drugs: Drug[];
+
+
+    medicationId: string;
+    unit: string;
+    dosage: string;
+*/
