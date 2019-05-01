1bf35da1259f5ff95ddf2bd76933176db2722ae0
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-05-01 21:46:56 +0200

    add-practice

--- src/app/dashboard/Dok-practices/add-practice/add-practice.component.ts
+++ src/app/dashboard/Dok-practices/add-practice/add-practice.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { PracticesService } from './../../../services/practices.service';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { getCurrentUser } from 'src/app/shared';
+import { LoginService } from 'src/app/home/login';
 
 @Component({
   selector: 'app-add-practice',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-practice.component.scss']
 })
 export class AddPracticeComponent implements OnInit {
+  @Output() closeModalAction: EventEmitter<boolean> = new EventEmitter();
+  @Input() PatientId;
+  @Input() Name;
+  rForm: FormGroup;
 
-  constructor() { }
+  prescriptionGiven: string;
+  UserId: string ='';
+
+  constructor(
+    private fb: FormBuilder,
+    private authitacateService: LoginService,
+    private practicesService: PracticesService,
+  ) {}
 
   ngOnInit() {
+    this.UserId = this.authitacateService.getUser.UserId;
+    this.rForm = this.fb.group({
+      Name: [null, Validators.required],
+      Address: [null, Validators.required],
+      CreateUserId: [this.UserId, Validators.required],
+      StatusId: [1, Validators.required]
+    });
+
+    this.rForm.valueChanges.subscribe(data => {
+      console.log(data);
+    });
+  }
+  closeModal() {
+    this.closeModalAction.emit(false);
   }
 
+  addPractice(data) {
+
+    this.practicesService.addPractice(data).subscribe(response => {
+      if (response.PracticeId) {
+        this.closeModalAction.emit(false);
+      } else {
+        alert(`Error: ${response}`);
+      }
+    });
+  }
 }
